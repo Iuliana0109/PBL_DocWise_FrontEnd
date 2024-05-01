f3aa72380bc347f3540ee1db7ca75d59dd728738
Iuliancik <68803808+Iuliana0109@users.noreply.github.com>
2024-05-01 15:31:09 +0300

    Update PillTable.vue for symtomps logs

--- components/dashboard/PillTable.vue
+++ components/dashboard/PillTable.vue
@@ -1,388 +1,179 @@
-<script setup lang="ts">
-import {productPerformance} from '@/data/dashboard/dashboardData';
-import { ref } from 'vue';
-
-interface NewRowData {
- name: string;
- time: string;
- quantity: number;
-}
-
-const newRows = ref<NewRowData[]>([]);
-    const newRowData = ref<NewRowData>({ name: '', time: '', quantity: 0 }); // Define newRowData here
-
-const addNewRow = (newRowData: NewRowData) => {
- newRows.value.push(newRowData);
-};
-</script>
 <template>
-    <v-card elevation="10" class="">
-        <v-card-item class="pa-6">
-        <v-card-title class="text-h3 pt-sm-2 pb-7">Pill Reminder</v-card-title>
-        <v-table class="month-table">
-            <thead>
-                <tr>
-                    <th class="text-subtitle-1 font-weight-bold">Id</th>
-                    <th class="text-subtitle-1 font-weight-bold">Pill Name</th>
-                    <th class="text-subtitle-1 font-weight-bold">Time</th>
-                    <th class="text-subtitle-1 font-weight-bold">Quantity</th>
-=                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in productPerformance" :key="item.name" class="month-item">
-                    <td>
-                        <p class="text-15 font-weight-medium">{{ item.id }}</p>
-                    </td>
-                    <td>
-                        <div class="">
-                            <h6 class="text-subtitle-1 font-weight-bold">{{ item.name }}</h6>
-                        </div>
-                    </td>
-                    <td>
-                        <h6 class="text-body-1 text-muted">{{ item.time }}</h6>
-                    </td>
-                    <td>
-                        <h6 class="text-body-1 text-muted">{{ item.quantity }}</h6>
-                    </td>
-                </tr>
-            </tbody>
-        </v-table>
-        </v-card-item>
-    </v-card>
+  <v-container style="max-width: 1200px;">
+    <v-timeline
+      density="compact"
+      side="end"
+    >
+      <v-timeline-item>
+        <v-text-field
+          v-model="input"
+          density="comfortable"
+          label="Leave a comment..."
+          hide-details
+          @keydown.enter="comment"
+        >
+          <template v-slot:append>
+            <v-btn
+              class="mx-0"
+              variant="text"
+              @click="comment"
+            >
+              Post
+            </v-btn>
+          </template>
+        </v-text-field>
+      </v-timeline-item>
 
-    <div>
-        <v-card-item class="pa-6">
-        <input v-model="newRowData.name" placeholder="Pill Name">
-        <input v-model="newRowData.time" placeholder="Time">
-        <input v-model="newRowData.quantity" placeholder="Quantity">
-        <button @click="addNewRow(newRowData)">Add Row</button>
-      </v-card-item>
-    </div>
+      <v-slide-x-transition group>
+        <v-timeline-item
+          v-for="event in timeline"
+          :key="event.id"
+          class="mb-4"
+          dot-color="primary"
+          size="small"
+        >
+          <div class="d-flex justify-space-between flex-grow-1">
+            <div>{{ event.text }}</div>
+            <div class="flex-shrink-0">  | {{ event.time }}</div>
+          </div>
+        </v-timeline-item>
+      </v-slide-x-transition>
+
+      <v-timeline-item
+        class="mb-6"
+        hide-dot
+      >
+        <span>YESTERDAY</span>
+      </v-timeline-item>
+
+      <v-timeline-item
+        class="mb-4"
+        dot-color="primary"
+        size="small"
+      >
+        <div class="d-flex justify-space-between flex-grow-1">
+          <div>
+            Headache
+          </div>
+          <div class="flex-shrink-0">
+            15:26 EDT
+          </div>
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item
+        class="mb-4"
+        dot-color="primary"
+        size="small"
+      >
+        <div class="d-flex justify-space-between flex-grow-1">
+          <div>
+            Headache
+          </div>
+          <div class="flex-shrink-0">
+            15:25 EDT
+          </div>
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item
+        class="mb-4"
+        dot-color="primary"
+        size="small"
+      >
+        <div class="d-flex justify-space-between flex-grow-1">
+          <div>
+            Chest Pain
+          </div>
+          <div class="flex-shrink-0">
+            15:25 EDT
+          </div>
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item
+        class="mb-4"
+        dot-color="primary"
+        size="small"
+      >
+        <div class="d-flex justify-space-between flex-grow-1">
+          <div>
+            Headache
+          </div>
+          <div class="flex-shrink-0">
+            15:25 EDT
+          </div>
+        </div>
+      </v-timeline-item>
+
+      <v-timeline-item
+        dot-color="primary"
+        size="small"
+      >
+        <div class="d-flex justify-space-between flex-grow-1">
+          <div>
+            Chest Pain
+          </div>
+          <div class="flex-shrink-0">
+            15:25 EDT
+          </div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
 
-<!-- 
-<template>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :sort-by="[{ key: 'calories', order: 'asc' }]"
-    >
-      <template v-slot:top>
-        <v-toolbar
-          flat
-        >
-          <v-toolbar-title>My CRUD</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog
-            v-model="dialog"
-            max-width="500px"
-          >
-            <template v-slot:activator="{ props }">
-              <v-btn
-                class="mb-2"
-                color="primary"
-                dark
-                v-bind="props"
-              >
-                New Item
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-  
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="Dessert name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.calories"
-                        label="Calories"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="blue-darken-1"
-                  variant="text"
-                  @click="close"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  color="blue-darken-1"
-                  variant="text"
-                  @click="save"
-                >
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon
-          class="me-2"
-          size="small"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          size="small"
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn
-          color="primary"
-          @click="initialize"
-        >
-          Reset
-        </v-btn>
-      </template>
-    </v-data-table>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        dialog: false,
-        dialogDelete: false,
-        headers: [
-          {
-            title: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            key: 'name',
-          },
-          { title: 'Calories', key: 'calories' },
-          { title: 'Fat (g)', key: 'fat' },
-          { title: 'Carbs (g)', key: 'carbs' },
-          { title: 'Protein (g)', key: 'protein' },
-          { title: 'Actions', key: 'actions', sortable: false },
-        ],
-        desserts: [],
-        editedIndex: -1,
-        editedItem: {
-          name: '',
-          calories: 0,
-          fat: 0,
-          carbs: 0,
-          protein: 0,
-        },
-        defaultItem: {
-          name: '',
-          calories: 0,
-          fat: 0,
-          carbs: 0,
-          protein: 0,
-        },
+<script setup>
+  import { computed, ref } from 'vue'
+
+  const events = ref([])
+  const input = ref(null)
+  const nonce = ref(0)
+
+  const timeline = computed(() => {
+    return events.value.slice().reverse()
+  })
+
+  function comment () {
+    const time = (new Date()).toTimeString()
+    events.value.push({
+      id: nonce.value++,
+      text: input.value,
+      time: time.replace(/^(\d{2}:\d{2}).*$/, '$1', (match, contents, offset) => {
+        return ` ${contents.split(' ').map(v => v.charAt(0)).join('')}`
       }),
-  
-      computed: {
-        formTitle () {
-          return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-        },
+    })
+    input.value = null
+  }
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      events: [],
+      input: null,
+      nonce: 0,
+    }),
+
+    computed: {
+      timeline () {
+        return this.events.slice().reverse()
       },
-  
-      watch: {
-        dialog (val) {
-          val || this.close()
-        },
-        dialogDelete (val) {
-          val || this.closeDelete()
-        },
+    },
+
+    methods: {
+      comment () {
+        const time = (new Date()).toTimeString()
+        this.events.push({
+          id: this.nonce++,
+          text: this.input,
+          time: time.replace(/:\d{2}\sGMT-\d{4}\s\((.*)\)/, (match, contents, offset) => {
+            return ` ${contents.split(' ').map(v => v.charAt(0)).join('')}`
+          }),
+        })
+
+        this.input = null
       },
-  
-      created () {
-        this.initialize()
-      },
-  
-      methods: {
-        initialize () {
-          this.desserts = [
-            {
-              name: 'Frozen Yogurt',
-              calories: 159,
-              fat: 6.0,
-              carbs: 24,
-              protein: 4.0,
-            },
-            {
-              name: 'Ice cream sandwich',
-              calories: 237,
-              fat: 9.0,
-              carbs: 37,
-              protein: 4.3,
-            },
-            {
-              name: 'Eclair',
-              calories: 262,
-              fat: 16.0,
-              carbs: 23,
-              protein: 6.0,
-            },
-            {
-              name: 'Cupcake',
-              calories: 305,
-              fat: 3.7,
-              carbs: 67,
-              protein: 4.3,
-            },
-            {
-              name: 'Gingerbread',
-              calories: 356,
-              fat: 16.0,
-              carbs: 49,
-              protein: 3.9,
-            },
-            {
-              name: 'Jelly bean',
-              calories: 375,
-              fat: 0.0,
-              carbs: 94,
-              protein: 0.0,
-            },
-            {
-              name: 'Lollipop',
-              calories: 392,
-              fat: 0.2,
-              carbs: 98,
-              protein: 0,
-            },
-            {
-              name: 'Honeycomb',
-              calories: 408,
-              fat: 3.2,
-              carbs: 87,
-              protein: 6.5,
-            },
-            {
-              name: 'Donut',
-              calories: 452,
-              fat: 25.0,
-              carbs: 51,
-              protein: 4.9,
-            },
-            {
-              name: 'KitKat',
-              calories: 518,
-              fat: 26.0,
-              carbs: 65,
-              protein: 7,
-            },
-          ]
-        },
-  
-        editItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialog = true
-        },
-  
-        deleteItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialogDelete = true
-        },
-  
-        deleteItemConfirm () {
-          this.desserts.splice(this.editedIndex, 1)
-          this.closeDelete()
-        },
-  
-        close () {
-          this.dialog = false
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem)
-            this.editedIndex = -1
-          })
-        },
-  
-        closeDelete () {
-          this.dialogDelete = false
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem)
-            this.editedIndex = -1
-          })
-        },
-  
-        save () {
-          if (this.editedIndex > -1) {
-            Object.assign(this.desserts[this.editedIndex], this.editedItem)
-          } else {
-            this.desserts.push(this.editedItem)
-          }
-          this.close()
-        },
-      },
-    }
-  </script> -->
+    },
+  }
+</script>
